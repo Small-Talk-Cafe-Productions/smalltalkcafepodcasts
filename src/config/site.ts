bb9033037ab5a1d3421f9844f7753c1317dfc017
Small Talk Café Productions <smalltalkcafe.productions@gmail.com>
2026-03-10 23:14:19 +0100

    Update site.ts

--- src/config/site.ts
+++ src/config/site.ts
@@ -103,7 +103,7 @@ export const siteConfig: SiteConfig = {
       'meaningful dialogue',
     ],
     ogImage: 'https://Small-Talk-Cafe-Productions.github.io/smalltalkcafepodcasts/images/og-image.jpg',
-    siteUrl: 'https://smalltalkcafe.de',
+    siteUrl: 'Small-Talk-Cafe-Productions.github.io/smalltalkcafepodcasts',
     twitterHandle: 'smalltalkcafe',
   },
   
